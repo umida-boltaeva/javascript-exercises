c4e9d4fbb84192884a5c68176fca7a69dbbc73ec
Umida Boltaeva <umidakhon.boltaeva@gmail.com>
2022-02-21 21:47:01 +1100

    check the palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let regex = /[\W_]/g;
+  let lowerRegStr = str.toLowerCase().replace(regex, '');
+  let reverseStr = lowerRegStr.split('').reverse().join('');
+  return lowerRegStr === reverseStr;
 };
 
 // Do not edit below this line
